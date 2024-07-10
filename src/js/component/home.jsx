53a9e17d446106a524b1ca5f99167dce2828b807
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-07-10 17:35:02 +0000

    Ejercicio completado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,28 +12,46 @@ const usableItems = {
 
 
 const Navbar = () => {
+
 	return (
-		<div>
-			<nav className="navbar navbar-expand-lg bg-dark p-0">
+		<>
+			<nav className="navbar navbar-dark bg-dark navbar-expand-lg py-1" id="navbarPrincipal" data-bs-theme="dark">
 				<div className="container-fluid d-flex justify-content-between">
-					<div className="container d-flex justify-content-start">
-						<a className="navbar-brand text-white" href="#">Navbar</a>
-						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-							<span className="navbar-toggler-icon"></span>
-						</button>
+					<button
+						className="navbar-toggler text-body-white"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNav"
+						aria-controls="navbarNav"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+						id="togglerButtonNavbar"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse text-white">
+						<span>Start Bootstrap</span>
 					</div>
-					<div className="container collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
-						<div className="navbar-nav" >
-							<a className="nav-link text-white" aria-current="page" href="#">Home</a>
-							<a className="nav-link text-secondary" href="#">About</a>
-							<a className="nav-link text-secondary" href="#">Services</a>
-							<a className="nav-link text-secondary" href="#">Contact</a>
-						</div>
+					<div className="collapse navbar-collapse text-lg-center ms-2 mt-2 p-0 row" id="navbarNav">
+						<ul className="navbar-nav d-flex justify-content-end">
+							<li className="nav-item mx-3">
+								<a href="/Home" className="nav-link active">Home</a>
+							</li>
+							<li className="nav-item mx-3">
+							<a href="/Home" className="nav-link">About Us</a>
+							</li>
+							<li className="nav-item mx-3">
+							<a href="/Services" className="nav-link">Services</a>
+							</li>
+							<li className="nav-item mx-3">
+							<a href="/Contact" className="nav-link">Contact</a>			
+							</li>
+						</ul>
 					</div>
 				</div>
 			</nav>
-		</div>
-	)
+		</>
+	);
 }
 
 const Jumbotron = () => {
@@ -48,7 +66,7 @@ const Jumbotron = () => {
 
 const Card = () => {
 	return (
-		<div className="card" style={{ width: '18rem' }}>
+		<div className="card my-3" style={{ width: '18rem' }}>
 			<img src="https://i.pinimg.com/564x/e6/c3/4a/e6c34afdf235e76c31344d6a933cae27.jpg" className="card-img-top" alt="Aquí debe aparecer una imagen" />
 			<div className="card-body">
 				<h5 className="card-title">Card title</h5>
@@ -63,12 +81,12 @@ const Card = () => {
 
 const Footer = () => {
 	return (
-			<footer class="text-center bg-dark">
-				<div className="text-center py-2 text-white">
-					© 2021 Copyright:
-					<a className="text-reset fw-bold ms-2" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-				</div>
-			</footer>
+		<footer class="text-center bg-dark mt-5">
+			<div className="text-center py-2 text-white">
+				© 2021 Copyright:
+				<a className="text-reset fw-bold ms-2" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+			</div>
+		</footer>
 	)
 }
 
@@ -79,21 +97,21 @@ const Home = () => {
 			<div className="d-flex justify-content-center">
 				<Jumbotron />
 			</div>
-			<div className="row my-5 mx-3">
-				<div className="col-md-3 d-flex justify-content-center">
+			<div className="row my-4 mx-3">
+				<div className="col-md-6 col-lg-3 d-flex justify-content-center">
 					<Card />
 				</div>
-				<div className="col-md-3 d-flex justify-content-center">
+				<div className="col-md-6 col-lg-3 d-flex justify-content-center">
 					<Card />
 				</div>
-				<div className="col-md-3 d-flex justify-content-center">
+				<div className="col-md-6 col-lg-3 d-flex justify-content-center">
 					<Card />
 				</div>
-				<div className="col-md-3 d-flex justify-content-center">
+				<div className="col-md-6 col-lg-3 d-flex justify-content-center">
 					<Card />
 				</div>
 			</div>
-			<div className="container-fluid p-0 m-0" id="footer">
+			<div className="container-fluid p-0 mx-0" id="footer">
 				<Footer />
 			</div>
 		</div>
